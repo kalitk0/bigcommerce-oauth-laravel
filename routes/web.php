df66b23def8c558ecbec6b014dfaeb7d163a38a8
kalitk0 <gatevv@gmail.com>
2025-03-30 13:41:53 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Route;
 
 Route::group([
-    'prefix' => 'auth'
+    'prefix' => 'bc/auth'
 ], function () {
 
     Route::get('install', [Config::get('bigcommerce-auth.controllers.install'), 'install'])
